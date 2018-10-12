2d8b963f2dd2f3b7302f84ff813d1f1b55f0c9e6
Prince Kumar <vividapps.in@gmail.com>
2018-10-12 15:28:48 +0530

    added delete item functionality to the cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatTable} from '@angular/material';
 
 @Component({
   selector: 'app-cart',
@@ -8,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class CartComponent implements OnInit {
   data;
   dataSource;
-  displayedColumns: string[] = ['id', 'name', 'price', 'quantity'];
+  tempcartitems;
+  displayedColumns: string[] = ['id', 'name', 'price', 'quantity', 'remove'];
+
+  @ViewChild(MatTable) table: MatTable<any>;
 
   constructor() { }
 
@@ -20,6 +24,26 @@ export class CartComponent implements OnInit {
 
     this.dataSource = this.data.items;
     console.log(this.dataSource);
+  }
+
+  /** Gets the total cost of all transactions. */
+  getTotalCost() {
+    return this.dataSource.map(t => t.price).reduce((acc, value) => acc + value, 0);
+  }
+
+  removeAll() {
+    this.dataSource.data.length = 0;
+    //this.table.renderRows();
+  }
+
+  removeAt(index: number) {
+    console.log(this.data.items);
+    this.data.items.splice(index, 1);
+    console.log(this.data.items);
+    this.tempcartitems = JSON.stringify(this.data);
+    localStorage.setItem('cartitems', this.tempcartitems ); // deleted from local storage by update it
+    this.dataSource = this.data.items; // deleted and updated dataSource for table
+    this.table.renderRows();
   }
 
 }
